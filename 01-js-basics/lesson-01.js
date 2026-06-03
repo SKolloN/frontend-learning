56d14f58458c47c9c20776a605b60354e5645e8b
SKolloN <skollon@mail.ru>
2026-06-04 03:23:22 +0700

    Add lesson 01 and git attributes

---
+++ 01-js-basics/lesson-01.js
@@ -0,0 +1,9 @@
+const productTitle = "Naruto Figure";
+const productPrice = "23000";
+const productCategory = "anime";
+const isAvailable = true;
+
+console.log(productTitle);
+console.log(productPrice);
+console.log(productCategory);
+console.log(isAvailable);
